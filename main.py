3b5b3a75973e96b487a19b4a272371710ee4c621
Error1234543 <ptaviyad21@gmail.com>
2025-06-14 14:53:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+API_ID = int(os.getenv("20619533"))
+API_HASH = os.getenv("5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = os.getenv("7764590689:AAFc4kG8_8hBRjye9MdsMndgwTfEPisSohE")
+OWNER_ID = int(os.getenv("7447651332"))
 ALLOWED_GROUP = int(os.getenv("ALLOWED_GROUP"))
 
 app = Client("Solo-Leech-Bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
